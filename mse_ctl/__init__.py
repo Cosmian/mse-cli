720ae8ecb21f40877c799454f68eb4f73f694ec9
Sébastien Lambert <sebastien.lambert@cosmian.com>
2022-11-03 12:09:38 +0100

    :truck: move global variables

--- mse_ctl/__init__.py
+++ mse_ctl/__init__.py
@@ -4,4 +4,12 @@ import os
 from pathlib import Path
 
 # TODO: deal with Windows and so on
-MSE_CONF_DIR = Path(os.getenv('MSE_CTL_CONF_PATH', "~/.config/mse-ctl/")).expanduser()
+# TODO: create $MSE_CTL_CONF_PATH if not exist
+MSE_CONF_DIR = Path(os.getenv("MSE_CTL_CONF_PATH",
+                              "~/.config/mse-ctl/")).expanduser()
+MSE_CERTIFICATES_URL = "https://certificates.cosmian.com/"
+MSE_PCCS_URL = "https://pccs.cosmian.com"
+MSE_DOCKER_IMAGE_URL = os.getenv("MSE_CTL_DOCKER_REMOTE_URL",
+                                 "gitlab.cosmian.com:5000/core/mse-docker")
+MSE_BACKEND_URL = os.getenv('MSE_CTL_BASE_URL',
+                            default="https://backend.mse.cosmian.com")
