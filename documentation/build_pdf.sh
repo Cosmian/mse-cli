5993aa9b27f9d39f252a3ca9d9defa3bf6cc3183
Sébastien Lambert <sebastien.lambert@cosmian.com>
2023-07-13 16:57:18 +0200

    :memo: make the doc pdf

--- documentation/build_pdf.sh
+++ documentation/build_pdf.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
 
-# Pre-requisites:
-    # sudo apt-get install texlive-full
-    # wget https://github.com/jgm/pandoc/releases/download/2.17.1.1/pandoc-2.17.1.1-1-amd64.deb
-    # sudo dpkg -i pandoc-2.17.1.1-1-amd64.deb
-    # sudo apt-get install fonts-noto-mono fonts-noto pandoc-citeproc librsvg2-bin
-    # pip install pydoc-markdown git+https://github.com/twardoch/mkdocs-combine.git mkdocs-kroki-plugin mkdocs-material pandoc-latex-admonition install markdown-katex git+https://gitlab.com/myriacore/pandoc-kroki-filter.git
-    # cargo install pandoc-katex
+# This scripts builds the PDF for a single document
+# Usage: ./scripts/single_doc_build.sh [DOCUMENT ROOT]
+
+if [ -z "$1" ]; then
+    echo Please specify the root folder containing the mkdocs.yml file
+    exit 1
+fi
+
+working_folder=$1
 
 set -e
 
-doc="user_guide"
+# Pre-requisites:
+    # sudo dpkg -i pandoc-3.1.5-1-amd64.deb
+    # sudo apt-get install fonts-noto-mono fonts-noto pandoc-citeproc librsvg2-bin
+    # pip install pydoc-markdown git+https://github.com/twardoch/mkdocs-combine.git mkdocs-kroki-plugin mkdocs-material pandoc-latex-admonition install markdown-katex git+https://gitlab.com/myriacore/pandoc-kroki-filter.git
 
-root=$1
-if [ -z "$1" ]
-then
-    root="."
-fi
+branch_tag="${CI_COMMIT_REF_NAME:-main}"
+echo "Building documentation for branch/tag: ${branch_tag}"
+
+# eisvogel_template=$(realpath pandoc/eisvogel.tex)
+working_dir=$(pwd)
+eisvogel_template="${working_dir}/pandoc/eisvogel.tex"
+
+rm -fr doc_build
 
 common_sed()
 {
@@ -30,22 +38,16 @@ common_sed()
         # echo "Not Linux"
         sed -i '' "${args}" "$file"
     fi
-
 }
 
-
-root=$(dirname "$1")
-script_dir=$(dirname "${BASH_SOURCE[0]}")
-script_dir=$(cd "$(dirname "$script_dir")"; pwd -P)/$(basename "$script_dir")
-
-eisvogel_template="${script_dir}/pandoc/eisvogel.tex"
-
-# echo "$eisvogel_template"
-# exit 0
+ # Set root folder and working folder
+doc=$(basename "$working_folder")
+root=$(dirname "$working_folder")
+output="doc"
 
 # Merge .md files
 # command added to build site directory to fix images displaying issue
-pushd "${root}"
+pushd "${root}/${doc}"
 if [[ -f "mkdocs.yml" ]]
 then
     echo "mkdocs.yml exists in folder ${doc}"
@@ -54,39 +56,40 @@ then
     mkdocs build
 
     # merge .md files into one .pd file
-    mkdocscombine -o "${doc}.pd"
+    mkdocscombine -o "${output}.pd"
 
     # Remove css classes added by mkdocscombine
-    common_sed "s/{: .page-title}/ /g" "${doc}.pd"
+    common_sed "s/{: .page-title}/ /g" "${output}.pd"
 
-    # TODO : Changes must be done in .md files not in .pd
     # Replace $`...`$ by $...$
-    # common_sed "s/$\`/$/g" "${doc}.pd"
-    # common_sed "s/\`\\$/$/g" "${doc}.pd"
+    #common_sed "s/$\`/$/g" "${output}.pd"
+    #common_sed "s/\`\\$/$/g" "${output}.pd"
 
-    # Replace HTML by Pandoc syntax
-    common_sed 's/<div class="admonition error">/::: {.admonition .error} :::/g' "${doc}.pd"
-    common_sed 's/<div class="admonition info">/::: {.admonition .info} :::/g' "${doc}.pd"
-    common_sed 's/<div class="admonition note">/::: {.admonition .note} :::/g' "${doc}.pd"
-    common_sed 's/<div class="admonition important">/::: {.admonition .important} :::/g' "${doc}.pd"
-    common_sed 's/<\/div>/:::::::::::::::::::::::::::::/g' "${doc}.pd"
-    common_sed 's/<h4>/**/g' "${doc}.pd"
-    common_sed 's/<\/h4>/**/g' "${doc}.pd"
-    common_sed 's/<br>/\\/g' "${doc}.pd"
+    # Replace ```math ... ```by $$...$$
+    perl -i  -0pe "s/(\`\`\`math)(.+?)(\`\`\`)/\\$\\$\2\\$\\$/smg" "${output}.pd"
+
+    # Replace !!! warning ... by \begin{warning}...\end{warning}
+    perl -i  -0pe "s/(!!! warning \"[^\"\n]+\"\n\n)(.+?)\n/\\\begin{warning}\2\\\end{warning}\n/smg" "${output}.pd"
+
+    # Replace !!! danger ... by \begin{danger}...\end{danger}
+    perl -i  -0pe "s/(!!! danger \"[^\"\n]+\"\n\n)(.+?)\n/\\\begin{dang}\2\\\end{dang}\n/smg" "${output}.pd"
+
+    # Replace !!! info ... by \begin{note}...\end{note}
+    perl -i  -0pe "s/(!!! info \"[^\"\n]+\"\n\n)(.+?)\n/\\\begin{note}\2\\\end{note}\n/smg" "${output}.pd"
+
+    # Replace !!! example ... by \begin{example}...\end{example}
+    perl -i  -0pe "s/(!!! Examples \"[^\"\n]+\"\n\n)(.+?)\n/\\\begin{ex}\2\\\end{ex}\n/smg" "${output}.pd"
 
     # Replace markdown tab syntax for Pandoc
-    grep -n '=== "' "${doc}.pd" | cut -f1 -d: | while read -r source ; do
-        echo - "$source"
-        common_sed "${source}s/===/#####/g" "${doc}.pd"
-        common_sed "${source}s/\"//g" "${doc}.pd"
+    grep -n '=== "' "${output}.pd" | cut -f1 -d: | while read -r source ; do
+        #echo - "$source"
+        common_sed "${source}s/===/#####/g" "${output}.pd"
+        common_sed "${source}s/\"//g" "${output}.pd"
     done
 
     # generate PDF
-    pdf_title="${doc}"
-    pandoc  --from markdown --template="${eisvogel_template}" includes.yml "${doc}.pd" -s  -o "${pdf_title}.pdf" --listings --pdf-engine=xelatex --filter pandoc-kroki #--filter pandoc-katex
+    pdf_title="${output}"
+    pandoc --citeproc --from markdown --include-before-body=pandoc/macros.tex --template="${eisvogel_template}" includes.yml "${output}.pd" -s  -o "${pdf_title}.pdf" --listings --pdf-engine=xelatex --filter pandoc-kroki --number-sections -M reference-section-title=References 
 
     rm -r site/
-else
- echo "no mkdocs.yml file in the $1 directory"
 fi
-popd
